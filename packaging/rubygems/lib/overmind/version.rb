136b6b24c43a74f73efce60c79721f5fa64ae182
hh1812906-droid <hh1812906@gmail.com>
2025-09-28 00:21:30 +0300

    Delete packaging directory
    packaging/tmux/README.md

--- packaging/rubygems/lib/overmind/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Overmind
-  VERSION = "2.5.1"
-end
--- packaging/rubygems/spec/dummy/server.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-puts "Hello, World! Server started."
-puts "Server is running..."
